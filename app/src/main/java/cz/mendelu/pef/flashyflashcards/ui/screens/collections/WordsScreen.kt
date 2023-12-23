91654df42aecdda1f5adbc2a13df3f90b78c7f2b
Daniil Astapenko <astapenko.daniil.03@gmail.com>
2023-12-23 20:38:05 +0100

    Replaced word test icon on WordsScreen with context menu with morevert icon (word test and test history will be there)

--- app/src/main/java/cz/mendelu/pef/flashyflashcards/ui/screens/collections/WordsScreen.kt
+++ app/src/main/java/cz/mendelu/pef/flashyflashcards/ui/screens/collections/WordsScreen.kt
@@ -1,19 +1,29 @@
 package cz.mendelu.pef.flashyflashcards.ui.screens.collections
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Quiz
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -59,6 +69,8 @@ fun WordsScreen(
         .currentBackStackEntry
         ?.savedStateHandle
         ?.get<String>(savedStateHandleKey)
+
+    var popupMenuExpanded by remember { mutableStateOf(false) }
 
     BasicScaffold(
         topAppBarTitle = updatedCollectionName ?: collectionName,
@@ -115,16 +127,11 @@ fun WordsScreen(
             }
 
             IconButton(onClick = {
-                navController.navigate(
-                    FlashcardPracticeScreenDestination(
-                        collectionId = collectionId,
-                        flashcardPracticeType = FlashcardPracticeType.Test(testAnswerLength)
-                    )
-                )
+                popupMenuExpanded = true
             }) {
                 Icon(
-                    imageVector = Icons.Default.CheckCircle,
-                    contentDescription = stringResource(id = R.string.test_label)
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = stringResource(id = R.string.more_label)
                 )
             }
         }
@@ -134,6 +141,49 @@ fun WordsScreen(
             navController = navController,
             uiState = viewModel.uiState
         )
+
+        if (popupMenuExpanded) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(end = basicMargin())
+                    .wrapContentSize(Alignment.TopEnd)
+            ) {
+                DropdownMenu(
+                    expanded = popupMenuExpanded,
+                    onDismissRequest = {
+                        popupMenuExpanded = false
+                    }
+                ) {
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = stringResource(id = R.string.test_label))
+                        },
+                        onClick = {
+                            popupMenuExpanded = false
+                            
+                            navController.navigate(
+                                FlashcardPracticeScreenDestination(
+                                    collectionId = collectionId,
+                                    flashcardPracticeType = FlashcardPracticeType.Test(
+                                        testAnswerLength
+                                    )
+                                )
+                            )
+                        }
+                    )
+
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = stringResource(id = R.string.test_history_label))
+                        },
+                        onClick = {
+
+                        }
+                    )
+                }
+            }
+        }
     }
 }
 
